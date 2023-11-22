5f5dbe8c2b54098daa385161f16cb41a1d5543b4
Bilal <bilal@grayphite.com>
2023-11-22 11:16:13 +0500

    First Commit

--- APIs/admin.py
+++ APIs/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
 from .models import Student, Instructor, Enrollment, Course
+
 # Register your models here.
 admin.register([Student, Instructor, Enrollment, Course])
