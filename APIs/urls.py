6e5c8a8fb019a5f30cac437d017e841fa5a21a7d
Usman <usman.grayphite@gmail.com>
2023-11-22 11:16:13 +0500

    First Commit

--- APIs/urls.py
+++ APIs/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
 from .views import CourseViewSet, StudentViewSet, EnrolmentCreateView, InstructorsListView, InstructorsDetailView
 
 router = DefaultRouter()
--- APIs/views.py
+++ APIs/views.py
@@ -1,9 +1,10 @@
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics
+from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.views import APIView
-from drf_yasg.utils import swagger_auto_schema
+
 from .models import Course, Student, Instructor
 from .serializers import CourseSerializer, StudentSerializer, EnrollmentSerializer, InstructorSerializer
 
